f6aa2354ea84d146df59297ca3b5467d8f0f7842
dy <844723655@qq.com>
2015-05-12 15:00:07 +0800

    main第一次提交

---
+++ src/com/daiyong/git2/GitTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015 智联招聘-Zhaopin.com
+ * 
+ * From project git-demo2
+ */
+
+package com.daiyong.git2;
+
+/**
+ * @author 代勇<yong.dai@zhaopin.com.cn>
+ * @Date 2015-5-12
+ */
+public class GitTest {
+
+	/**
+	 * 
+	 * @author 代勇<yong.dai@zhaopin.com.cn>
+	 */
+	private void mian() {
+		
+	}
+}
